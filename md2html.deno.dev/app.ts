1264380e76accd6ce9b9b7f1c2c5bbd26c99a918
Kuss <kussssss@outlook.com>
2022-07-28 19:25:51 +0800

    refactor: md2html template.html

--- md2html.deno.dev/app.ts
+++ md2html.deno.dev/app.ts
@@ -1,54 +1,21 @@
 import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
 import { Marked } from 'https://deno.land/x/markdown@v2.0.0/mod.ts'
-
 import { path, dirname } from '../utils.ts';
 
 const README_PATH = path.join(dirname(import.meta.url), './README.md')
+
+const TPL_PATH = path.join(dirname(import.meta.url), './template.html')
+
+const TPL = await Deno.readTextFile(TPL_PATH)
 
 Marked.setOptions({
   langPrefix: 'shj-lang-'
 })
 
-const cssCDN = `
-<link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" media="screen" />
-<link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura-dark.css" media="screen and (prefers-color-scheme: dark)" />
-
-<link rel="stylesheet" href="https://unpkg.com/@speed-highlight/core/dist/themes/github-light.css" media="screen">
-<link rel="stylesheet" href="https://unpkg.com/@speed-highlight/core/dist/themes/github-dark.css" media="screen and (prefers-color-scheme: dark)">
-`
-
-const styles = `<style>
-
-@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@400&display=swap');
-
-body {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-pre {
-  border-radius: 1rem;
-}
-code {
-  font: 18px 'Fira Code', monospace !important
-}
-img {
-  margin: 0;
-}
-</style>`
-
-const scripts = `
-<script type="module">
-  import { highlightAll } from 'https://unpkg.com/@speed-highlight/core@1.1.7/dist/index.js';
-  highlightAll({
-    hideLineNumbers: false
-  });
-</script>
-`
 
 const toHTML = (md: string) => {
   const parsed = Marked.parse(md)
-  return `<head>${cssCDN}${styles}</head>` + `<body><div>${parsed.content}</div>${scripts}</body>`
+  return TPL.replace('{{content}}', parsed.content)
 }
 
 const RES_OPTIONS = {
