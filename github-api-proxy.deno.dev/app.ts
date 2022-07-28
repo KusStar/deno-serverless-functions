4d4b8f9d2a557f744e7b38fb039c0973c64837d1
Kuss <kussssss@outlook.com>
2022-07-28 13:34:51 +0800

    fix: github-api-proxy pathname

--- github-api-proxy.deno.dev/app.ts
+++ github-api-proxy.deno.dev/app.ts
@@ -6,7 +6,7 @@ export const README = path.join(rootDir, "./github-api-proxy.deno.dev/README.md"
 const MD_TO_HTML_API = Deno.env.get("DEV") ? 'http://localhost:3000' : 'https://md2html.deno.dev'
 
 async function handleRequest(request: Request) {
-  const { pathname } = new URL(request.url);
+  let { pathname } = new URL(request.url);
   if (pathname === "/") {
     const md = await Deno.readTextFile(README)
     const res = await fetch(MD_TO_HTML_API, {
@@ -16,7 +16,12 @@ async function handleRequest(request: Request) {
     return res;
   }
 
-  const url = new URL(pathname.slice(2), "https://api.github.com").toString()
+  if (pathname.startsWith('/') || pathname.startsWith('//')) {
+    pathname = pathname.slice(1);
+  }
+
+  const url = new URL(pathname, "https://api.github.com").toString()
+
   const res = await fetch(url, {
     headers: request.headers
   })
