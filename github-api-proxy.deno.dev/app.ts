d8c946c0d3446ac7c6d4af93ebe0546ac12fe130
Kuss <kussssss@outlook.com>
2022-08-16 18:59:08 +0800

    refactor: dummy github-api-proxy forward searchParams

--- github-api-proxy.deno.dev/app.ts
+++ github-api-proxy.deno.dev/app.ts
@@ -2,19 +2,17 @@ import { serve } from "https://deno.land/std@0.149.0/http/server.ts";
 import { fetchReadmeToHtml } from '../utils.ts';
 
 async function handleRequest(request: Request) {
-  let { pathname } = new URL(request.url);
-  if (pathname === "/") {
+  const url = new URL(request.url);
+  if (url.pathname === "/") {
     const res = await fetchReadmeToHtml(import.meta.url)
     return res;
   }
 
-  if (pathname.startsWith('/') || pathname.startsWith('//')) {
-    pathname = pathname.slice(1);
-  }
+  url.hostname = "api.github.com"
+  url.port = ""
+  url.protocol = "https:"
 
-  const url = new URL(pathname, "https://api.github.com").toString()
-
-  const res = await fetch(url, {
+  const res = await fetch(url.href, {
     method: request.method,
     headers: request.headers,
     body: request.body
